fe738314f9fb91882b789d4d3b3cd60936b5518d
kehwa <kehwaweng@staff.ipevo.com>
2021-05-06 15:25:10 +0800

    Initial Commit

---
+++ HungarianAlgorithm/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  HungarianAlgorithm
+//
+//  Created by kehwaweng on 2021/5/6.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
